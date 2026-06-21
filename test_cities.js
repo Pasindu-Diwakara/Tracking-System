4f13932befcf734993df1e3b15e409bf89a98de0
Pasindu Diwakara <75148784+Pasindu-Diwakara@users.noreply.github.com>
2026-06-21 18:59:47 +0530

    commit

--- test_cities.js
+++
@@ -1,7 +0,0 @@
-import { Country, City } from "country-state-city";
-let maxCities = 0;
-for (const c of Country.getAllCountries()) {
-  const cities = City.getCitiesOfCountry(c.isoCode);
-  if (cities.length > maxCities) maxCities = cities.length;
-}
-console.log("Max cities in a country:", maxCities);
